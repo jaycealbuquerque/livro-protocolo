291445e4465665538577e143ee29a4b9bb64f768
Jayce Albuquerque <jeycealbuquerquerocha@gmail.com>
2023-07-21 21:17:25 -0300

    delete directory

--- src/http/controller/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { CreateProtocoloController } from './create-protocolo'
-
-const createProtocolo = Router()
-
-createProtocolo.post('/createProtocolo', new CreateProtocoloController().handle)
-
-export { createProtocolo }
